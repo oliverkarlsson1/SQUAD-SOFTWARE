17e35f0a053d06c022a66c392d74d71429e87023
oliverkarlsson1 <oliverjckarlsson@gmail.com>
2024-02-16 15:28:31 +0100

    V1.1

--- main.py
+++ main.py
@@ -1,53 +1,15 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.stats import linregress
 
-try:
-    import scienceplots
-    plt.style.use(['science', 'grid'])
-except ImportError:
-    pass
+k = 1
+epsilon = 1
+E_min = -13*epsilon
 
-# Exercise 1
 table_1 = {
     "N": [20, 40, 80, 160],
     "ree_a_ratio_squared": [66.7, 193, 549, 1555]
 }
-
-total_steps = np.array(table_1["N"])
-ree_a_ratio_squared = np.array(table_1["ree_a_ratio_squared"])
-
-step_length = 1
-
-
-def ree_squared(n, a):
-    return n*a**2
-
-
-log_N = np.log(np.array(table_1["N"]))
-log_ree_a_ratio_squared = np.log(table_1["ree_a_ratio_squared"])
-v, _, _, _, _, = linregress(log_N, log_ree_a_ratio_squared)
-
-plt.figure(figsize=(10, 6))
-plt.plot(np.log(total_steps),
-         np.log(ree_a_ratio_squared),
-         label='Self-Avoiding Walk')
-plt.plot(np.log(total_steps),
-         np.log(ree_squared(total_steps, step_length)/(step_length**2)),
-         label='Ordinary Random Walk')
-plt.xlabel('$log(N)$',
-           fontsize=14)
-plt.ylabel('$log(r_{ee}^2/a^2)$',
-           fontsize=14)
-plt.title('Self-Avoiding Walk vs Ordinary Random Walk ')
-plt.legend()
-
-plt.text(3, 4, rf'$ \nu \approx {v/2:.2f}$',
-         fontsize=12)
-
-plt.show()
-
-# Exercise 2b
 table_2 = {
     "E/epsilon": [0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13],
     "gE": [18_671_059_783.5, 15_687_265_041, 5_351_538_782, 1_222_946_058,
@@ -56,64 +18,100 @@ table_2 = {
 }
 
 
-E_epsilon_ratio = np.array(table_2["E/epsilon"])
-print(len(E_epsilon_ratio))
-density_of_states = np.array(table_2["gE"])
+class Exercise1:
 
-temperature_values = np.linspace(0.1, 1, 100)
+    def __init__(self, data):
+        self.total_steps = np.array(data["N"])
+        self.ree_a_ratio_squared = np.array(data["ree_a_ratio_squared"])
 
-k = 1
-epsilon = 1
-E_min = -13*epsilon
+    def plot(self):
+        log_N = np.log(self.total_steps)
+        log_ree_a_ratio_squared = np.log(self.ree_a_ratio_squared)
+        v, _, _, _, _ = linregress(log_N, log_ree_a_ratio_squared)
 
-
-def average_energy(k, temperature):
-    numerator = np.sum(E_epsilon_ratio * density_of_states * np.exp(-(E_epsilon_ratio - E_min) / (k * temperature)))
-    denominator = np.sum(density_of_states * np.exp(-(E_epsilon_ratio - E_min) / (k * temperature)))
-    return numerator / denominator
-
-
-def average_energy_squared(k, temperature):
-    numerator = np.sum(E_epsilon_ratio ** 2 * density_of_states * np.exp(-(E_epsilon_ratio - E_min) / (k * temperature)))
-    denominator = np.sum(density_of_states * np.exp(-(E_epsilon_ratio - E_min) / (k * temperature)))
-    return numerator / denominator
-
-
-def heat_capacity(k, temperature):
-    return (1/(k*temperature**2)) * (average_energy_squared(k, temperature) - average_energy(k, temperature)**2)
+        plt.figure(figsize=(10, 6))
+        plt.plot(log_N, log_ree_a_ratio_squared, label='Self-Avoiding Walk')
+        plt.plot(log_N, log_N, label='Ordinary Random Walk')
+        plt.xlabel('$log(N)$', fontsize=14)
+        plt.ylabel('$log(r_{ee}^2/a^2)$', fontsize=14)
+        plt.title('Self-Avoiding Walk vs Ordinary Random Walk')
+        plt.legend()
+        plt.text(3, 4, rf'$ \nu \approx {v / 2:.2f}$', fontsize=12)
+        plt.savefig('exercise_1.png')
+        plt.show()
 
 
-CV_values = [heat_capacity(k, t) for t in temperature_values]
+class Exercise2b:
+    def __init__(self, data, k, epsilon):
+        self.E_min = -13 * epsilon
+        self.E_epsilon_ratio = np.array(data["E/epsilon"])
+        self.density_of_states = np.array(data["gE"])
+        self.temperature_values = np.linspace(0.1, 1, 100)
+        self.k = k
 
-plt.figure(figsize=(10, 6))
-plt.plot(temperature_values, CV_values)
-plt.xlabel('$T\ [\\epsilon/k]$',
-           fontsize=14)
-plt.ylabel('$C_V\ [k]$',
-           fontsize=14)
+    def average_energy(self, temperature):
+        numerator = np.sum(self.E_epsilon_ratio * self.density_of_states * np.exp(
+            -(self.E_epsilon_ratio - self.E_min) / (self.k * temperature)))
+        denominator = np.sum(
+            self.density_of_states * np.exp(-(self.E_epsilon_ratio - self.E_min) / (self.k * temperature)))
+        return numerator / denominator
 
-plt.title('Heat Capacity vs Temperature')
+    def average_energy_squared(self, temperature):
+        numerator = np.sum(self.E_epsilon_ratio ** 2 * self.density_of_states * np.exp(
+            -(self.E_epsilon_ratio - self.E_min) / (self.k * temperature)))
+        denominator = np.sum(
+            self.density_of_states * np.exp(-(self.E_epsilon_ratio - self.E_min) / (self.k * temperature)))
+        return numerator / denominator
 
-plt.show()
+    def heat_capacity(self, temperature):
+        return (1 / (self.k * temperature ** 2)) * (
+                    self.average_energy_squared(temperature) - self.average_energy(temperature) ** 2)
 
-T_max = temperature_values[np.argmax(CV_values)]
-print("Temperature at which CV is maximal:", T_max)
+    def plot(self):
+        cv_values = [self.heat_capacity(t) for t in self.temperature_values]
+        t_max = self.temperature_values[np.argmax(cv_values)]
 
-# Exercise 2c
-def probability(k, t):
-    numerator = np.exp(-E_min/(k*t))
-    denominator = np.sum(density_of_states * np.exp(-E_epsilon_ratio/(k*t)))
-    return numerator / denominator
+        plt.figure(figsize=(10, 6))
+        plt.plot(self.temperature_values, cv_values)
+        plt.xlabel('$T\ [\\epsilon/k]$', fontsize=14)
+        plt.ylabel('$C_V\ [k]$', fontsize=14)
+        plt.axvline(t_max, color='r', linestyle='--')
+        plt.text(0.43, 6, f'$T = {t_max:.3f} \ \\epsilon/k$', fontsize=12)
+        plt.title('Heat Capacity vs Temperature')
+        plt.savefig('exercise_2b.png')
+        plt.show()
+
+class Exercise2c:
+    def __init__(self, data, k, epsilon):
+        self.E_min = -13 * epsilon
+        self.E_epsilon_ratio = np.array(data["E/epsilon"])
+        self.density_of_states = np.array(data["gE"])
+        self.temperature_values = np.linspace(0.1, 1, 100)
+        self.k = k
+
+    def probability(self, t):
+        numerator = np.exp(-self.E_min / (self.k * t))
+        denominator = np.sum(self.density_of_states * np.exp(-self.E_epsilon_ratio / (self.k * t)))
+        return numerator / denominator
+
+    def plot(self):
+        P_values = [self.probability(t) for t in self.temperature_values]
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(self.temperature_values, P_values)
+        plt.xlabel('$T\ [\\epsilon/k]$', fontsize=14)
+        plt.ylabel('$P_{nat}$', fontsize=14)
+        plt.title('Probability of finding the chain in its minimum-energy state vs Temperature')
+        plt.savefig('exercise_2c.png')
+        plt.show()
 
 
-P_values = [probability(k, t) for t in temperature_values]
+# Instantiate and run each exercise
+exercise1 = Exercise1(table_1)
+exercise1.plot()
 
-plt.figure(figsize=(10, 6))
-plt.plot(temperature_values, P_values)
-plt.xlabel('$T\ [\\epsilon/k]$',
-           fontsize=14)
-plt.ylabel('$P_{nat}$',
-           fontsize=14)
-plt.title('Probability of finding the chain in its minimum-energy state vs Temperature')
+exercise2 = Exercise2b(table_2, k, epsilon)
+exercise2.plot()
 
-plt.show()
+exercise3 = Exercise2c(table_2, k, epsilon)
+exercise3.plot()
